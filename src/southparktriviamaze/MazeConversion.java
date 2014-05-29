e1701930624a1076095c8e7b6bd53c89f0aef0c4
cpurta <c_purta@hotmail.com>
2014-05-28 18:59:15 -0700

    Trying to get maze to print (in work)

--- src/southparktriviamaze/MazeConversion.java
+++ src/southparktriviamaze/MazeConversion.java
@@ -1,19 +1,52 @@
-	package southparktriviamaze;
+package southparktriviamaze;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 	
-	public class MazeConversion {
+	public class MazeConversion extends JPanel{
 		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		private static final int CORNER = 1;
 		private static final int ROOM = 0;
 		private static final int WALL = 1;
 		private static final int LOCKEDDOOR = 2;
 		private static final int UNLOCKEDDOOR = 3;
 		
+		private static final int pixelWidth = 100;
+		private static final int pixelLength = 100;
+		
+		
 		private Maze maze;
 		private int[][] array;
+		private Image room, wall, lockedDoor, unlockedDoor;
+		
+		public static void main(String [] args) throws Exception
+		{
+			Maze m = new Maze(5, 5);
+			MazeConversion mC = new MazeConversion(m);
+			
+			JFrame frame = new JFrame();
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setSize(600, 600);
+	        frame.add(mC);
+	        frame.setVisible(true);
+		}
 		
 		public MazeConversion(Maze m) {
 			this.maze = m;
 			convertMazeToChar();
+			
+			this.room = new ImageIcon("/Resources/Room.png").getImage();
+			this.wall = new ImageIcon("/Resources/Wall.png").getImage();
+			this.lockedDoor = new ImageIcon("/Resources/LockedDoor.png").getImage();
+			this.unlockedDoor = new ImageIcon("/Resources/UnlockedDoor.png").getImage();
 		}
 		
 		public int[][] convertedMaze()
@@ -24,7 +57,7 @@
 		private void convertMazeToChar() {
 			this.array = new int[2 * maze.getRows() + 1][2 * maze.getCols() + 1];
 			
-			this.array = initializeCharArray();
+			this.array = initializeIntArray();
 			
 			tearDownWallsToDoors();
 					
@@ -36,25 +69,25 @@
 				{
 					if(i % 2 == 1 && j % 2 == 1)
 					{
-						boolean[] neighboringRooms = this.maze.getNeighboringRooms(i/2, j/2);
+						boolean[] neighboringRooms = this.maze.getNeighboringRooms(i/2, j/2);//North, South, East, West
 						
 						if(neighboringRooms[0])//North
 							this.array[i - 1][j] = LOCKEDDOOR;
 						
-						else if(neighboringRooms[1])//South
+						if(neighboringRooms[1])//South
 							this.array[i + 1][j] = LOCKEDDOOR;
 						
-						else if(neighboringRooms[2])//East
+						if(neighboringRooms[2])//East
 							this.array[i][j + 1] = LOCKEDDOOR;
 						
-						else if(neighboringRooms[3])//West
+						if(neighboringRooms[3])//West
 							this.array[i][j - 1] = LOCKEDDOOR;
 					}
 					
 				}//end for j
 		}
 	
-		private int[][] initializeCharArray() {
+		private int[][] initializeIntArray() {
 			int [][] temp = new int[2 * maze.getRows() + 1][2 * maze.getCols() + 1];
 			
 			for(int i = 0; i < temp.length; i++)
@@ -92,5 +125,24 @@
 			}//end for i
 			
 		}//end printCharMaze
+		
+		public void paint(Graphics g)
+		{
+			super.paint(g);
+			
+			for(int i = 0; i < this.array.length; i++)
+				for(int j = 0; j < this.array[i].length; j++)
+				{
+					if(this.array[i][j] == CORNER || this.array[i][j] == WALL)
+						g.drawImage(wall, i * pixelWidth, j * pixelLength, null);
+					if(this.array[i][j] == ROOM)
+						g.drawImage(room, i * pixelWidth, j * pixelLength, null);
+					if(this.array[i][j] == LOCKEDDOOR)
+						g.drawImage(lockedDoor, i * pixelWidth, j * pixelLength, null);
+					if(this.array[i][j] == UNLOCKEDDOOR)
+						g.drawImage(unlockedDoor, i * pixelWidth, j * pixelLength, null);
+				}//end for j
+			
+		}//end paintComponent
 	
 	}
